35e192412ccb4a08dc192ffc871a3dfd80a77f6a
repl.it user <replituser@example.com>
2021-02-14 19:48:11 +0000

    few more practices on functions

--- src/funcptr.cpp
+++ src/funcptr.cpp
@@ -5,6 +5,25 @@ void sample2();
 
 int main()
 {
-  
+  void (*func_ptr[2])()= {nullptr};
+
+  func_ptr[0] = &sample1;
+
+  func_ptr[0]();
+
+  func_ptr[1] = sample2;
+
+  func_ptr[1]();
+
   return 0;
+}
+
+void sample1()
+{
+  puts("Sample 1");
+}
+
+void sample2()
+{
+  puts("Sample 2");
 }
--- src/typeid.cpp
+++ src/typeid.cpp
@@ -1,15 +1,63 @@
 #include <cstdio>
 #include <typeinfo>
 
+int test1(int);
+int test1(int, int);
+void test1();
+//int test1(); overload with different return types
+// compiler cannot figerout which version to use now
+
+class Sample 
+{
+public:
+  int test2(int a)
+  {
+    return a;
+  }
+
+  int test2(int a, int b)
+  {
+    return a + b;
+  }
+
+  int test3(int a)
+  {
+    return ++a;
+  }
+};
+
 int main()
 {
   int a;
   char b;
+  Sample s;
   printf("%s\n", typeid(double* []).name());
 
   if(typeid(a) != typeid(b))
   {
     puts("Yes, we're different!");
   }
+
+  a = test1(1);
+  a = test1(1,2);
+  test1();
+  //a = test1();
+
+  printf("%d\n",a);
+
+  printf("%d %d %d\n", s.test2(1), s.test2(1,2), s.test3(1));
+
   return 0;
 }
+
+int test1(int a)
+{
+  return ++a;
+}
+
+int test1(int a, int b)
+{
+  return a + b;
+}
+
+void test1() {}
